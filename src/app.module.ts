699d6e719e9b595273d084c2644268b0a7fa5d26
Boris Šuličenko <b.sulicenko@gmail.com>
2021-08-08 18:18:15 +0200

    #45

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typeOrmConfig } from './config/typeorm.config';
+import { CampaignModule } from './modules/campaign/campaign.module';
 
 @Module({
-  imports: [],
+  imports: [CampaignModule, TypeOrmModule.forRoot(typeOrmConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
